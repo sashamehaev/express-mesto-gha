626691ff763928301d7c2b504651340a6e0a7d17
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-06-27 16:48:38 +0300

    project 13

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.listen(PORT, () => {
+  const _id = '1231122212';
+  console.log(_id);
   // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
