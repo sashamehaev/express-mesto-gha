72fb84f9d1e4851c32392cd28f8b8e1af3196a5d
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-06-30 19:36:11 +0300

    project 13

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express');
-// Слушаем 3000 порт
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62bc84715bde0977268074ef',
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+
+app.use('/cards', require('./routes/cards'));
+
 app.listen(PORT, () => {
-  const _id = '1231122212';
-  console.log(_id);
-  // Если всё работает, консоль покажет, какой порт приложение слушает
   console.log(`App listening on port ${PORT}`);
 });
