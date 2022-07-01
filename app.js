c864416a090564215ebc08f1821f13b7d7f73fff
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-07-01 19:01:38 +0300

    project 13

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.use('/users', require('./routes/users'));
 
 app.use('/cards', require('./routes/cards'));
 
+app.use('/*', (req, res) => {
+  res.status(404).send({ message: 'Страницы не существует' });
+});
+
 app.listen(PORT, () => {
 
 });
