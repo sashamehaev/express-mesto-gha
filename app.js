41403670f4a87c6365c5bf3e512ccbad9c777799
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-07-05 19:35:57 +0300

    project 14

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().url(),
   }),
 }), createUser);
 
--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().url(),
   }),
 }), updateUser);
 router.patch('/me/avatar', updateAvatar);
