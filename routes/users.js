abf99d87543941d9f57ce2f718d7db8260460c3e
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-07-05 15:53:21 +0300

    project 14

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,13 @@ router.get('/:userId', celebrate({
     userId: Joi.string().length(24).hex(),
   }),
 }), getUserById);
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+  }),
+}), updateUser);
 router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
