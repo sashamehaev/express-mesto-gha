b5647a6163d4cd69135f6e4937c61745c3285fbd
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-07-05 15:57:42 +0300

    project 14

--- routes/cards.js
+++ routes/cards.js
@@ -11,12 +11,20 @@ const {
 router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
     link: Joi.string().required(),
   }),
 }), createCard);
 router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), dislikeCard);
 
 module.exports = router;
