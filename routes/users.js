962f91044bf591cfbbb90d6630d97302c488f922
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-07-05 13:27:10 +0300

    project 14

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers,
   getUserById,
@@ -9,7 +10,11 @@ const {
 
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
-router.get('/:userId', getUserById);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex(),
+  }),
+}), getUserById);
 router.patch('/me', updateUser);
 router.patch('/me/avatar', updateAvatar);
 
